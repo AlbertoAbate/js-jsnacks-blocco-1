ca5094618e16046db4b24b8fd18defe2567bec3a
Alberto <alberto.abate87@gmail.com>
2020-10-16 15:53:26 +0200

    jsnack1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// jsnack1
+var num1 = parseInt(prompt('scrivi un numero'));
+var num2 = parseInt(prompt('scrivi un altro numero'));
+
+if (num1 > num2) {
+  console.log(num1);
+}
+else if (num2 > num1) {
+  console.log(num2);
+} else {
+  console.log(pareggio);
+}
